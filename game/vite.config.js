dc698cd95ecb948ed50c8627a873b695675ed40b
JavidelaRubia <javidelarubia@gmail.com>
2025-01-16 10:27:23 +0100

    more-config-vite

--- game/vite.config.js
+++ game/vite.config.js
@@ -2,38 +2,42 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Mi PWA con Vite',
-        short_name: 'MiPWA',
-        description: 'Una aplicación web progresiva creada con Vite y JavaScript',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'test.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'test.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/jsonplaceholder\.typicode\.com\/.*$/,
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'api-cache',
+    base: '/',
+    build: {
+        outDir: 'dist',
+    },
+    plugins: [
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'Mi PWA con Vite',
+                short_name: 'MiPWA',
+                description: 'Una aplicación web progresiva creada con Vite y JavaScript',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'test.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'test.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
             },
-          },
-        ],
-      },
-    }),
-  ],
+            workbox: {
+                runtimeCaching: [
+                    {
+                        urlPattern: /^https:\/\/jsonplaceholder\.typicode\.com\/.*$/,
+                        handler: 'NetworkFirst',
+                        options: {
+                            cacheName: 'api-cache',
+                        },
+                    },
+                ],
+            },
+        }),
+    ],
 });
